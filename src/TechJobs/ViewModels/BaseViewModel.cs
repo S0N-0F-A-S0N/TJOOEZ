2634e3fa00d99914457131fb989a52a76061a879
S0N_0F_A_S0N <S0N-0F-A-S0N@users.noreply.github.com>
2018-01-11 13:15:38 -0600

    Added constructor

--- src/TechJobs/ViewModels/BaseViewModel.cs
+++ src/TechJobs/ViewModels/BaseViewModel.cs
@@ -9,6 +9,7 @@ namespace TechJobs.ViewModels
 {
     public class BaseViewModel : Controller
     {
+        public BaseViewModel() { }
         public JobFieldType Column { get; set; }
         public List<JobFieldType> Columns { get; set; }
         public string Title { get; set; } = "";
