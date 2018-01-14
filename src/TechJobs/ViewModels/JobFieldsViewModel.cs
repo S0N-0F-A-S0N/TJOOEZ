1b862d805a6f36c171d03bc9ea295967bdc1815a
S0N_0F_A_S0N <S0N-0F-A-S0N@users.noreply.github.com>
2018-01-13 19:52:30 -0600

    Removed Constructor

--- src/TechJobs/ViewModels/JobFieldsViewModel.cs
+++ src/TechJobs/ViewModels/JobFieldsViewModel.cs
@@ -7,12 +7,5 @@ namespace TechJobs.ViewModels
     public class JobFieldsViewModel : BaseViewModel
     {
         public IEnumerable<JobField> Fields { get; set; }
-        public JobFieldsViewModel()
-        {
-            Columns = new List<JobFieldType>();
-            foreach (JobFieldType enumVal in Enum.GetValues(typeof(JobFieldType))) {
-                Columns.Add(enumVal);
-            }
-        }
     }
 }
